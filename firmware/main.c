f69532889edd12721a1c605421139ce1854424cb
Henning Bekel <h.bekel@googlemail.com>
2015-07-22 20:59:33 +0200

    Removed unused uart stuff

--- firmware/main.c
+++ firmware/main.c
@@ -2,14 +2,20 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <avr/io.h>
+#include <avr/power.h>
+#include <util/delay.h>
 
 #define MC 1<<PA0
 #define MD 1<<PA1
 
-static volatile uint8_t matrix[8] = { 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
+static volatile uint8_t matrix[8] = {
+  0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+};
 
 static void SetupHardware(void) {
-  
+
+  clock_prescale_set(clock_div_1);
+
   // Control / Output 8-11
   DDRA  = 0b11110001;
   PORTA = 0b00001110;
@@ -25,10 +31,14 @@ static void SetupHardware(void) {
   // CIA1 PORT B
   DDRD  = 0b11111111;
   PORTD = 0b00000000;
+}
 
-  InitUART();
+static void LedOn(void) {
+  PORTC &= ~(1<<PINC0);
+}
 
-  printf("Hardware initialized\n");
+static void LedOff(void) {
+  PORTC |= (1<<PINC0);
 }
 
 static void ClockMatrix(void) {
@@ -51,24 +61,31 @@ static void DebugMatrix(void) {
 int main(void) {
   SetupHardware();
 
+  /*
   uint8_t row = 0;
   uint8_t col = 0;
-  
-  while(1) {
 
+  while(1) {
     for(row=0; row<8; row++) {
       for(col=0; col<8; col++) {
-
-	if((PINA & MD) == 0) {
-	  matrix[row] &= ~(1<<col);
-	}
-	else {
-	  matrix[row] |= 1<<col;
-	}
-	ClockMatrix();
+        
+        if((PINA & MD) == 0) {
+          matrix[row] &= ~(1<<col);
+        }
+        else {
+          matrix[row] |= 1<<col;
+        }
+        ClockMatrix();
       }
     }
     DebugMatrix();
-  }  
+  } 
+  */
+  while(1) {
+    LedOn();
+    _delay_ms(500);
+    LedOff();
+    _delay_ms(500);
+  }
   return 0;
 }
