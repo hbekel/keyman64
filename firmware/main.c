0e04adc199ea33786d9e0402a154946186ac07dd
Henning Bekel <h.bekel@googlemail.com>
2015-07-26 23:04:52 +0200

    Started implementing state machine and key handling for command key

--- firmware/main.c
+++ firmware/main.c
@@ -12,9 +12,16 @@
 #define MD 1<<PD4
 #define MR 1<<PD0
 
+#define RELAY   0x00
+#define COMMAND 0x01
+
+uint8_t STATE = RELAY;
+
 static volatile uint8_t matrix[8] = {
   0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
 };
+
+//------------------------------------------------------------------------------
 
 static void SetupHardware(void) {
 
@@ -39,15 +46,21 @@ static void SetupHardware(void) {
   PORTD = 0b11110110;
 }
 
+//------------------------------------------------------------------------------
+
 static void DisableJTAG(void) {
   MCUCR |= (1<<JTD);
   MCUCR |= (1<<JTD);
 }
 
-static void ResetMatrix(void) {
+//------------------------------------------------------------------------------
+
+static void ResetCounter(void) {
   PORTD |= MR;
   PORTD &= ~MR;
 }
+
+//------------------------------------------------------------------------------
 
 static void ClockMatrix(void) {
   PORTD |= MC;
@@ -55,16 +68,20 @@ static void ClockMatrix(void) {
   PORTD &= ~MC;
 }
 
-static void ScanMatrix(void) {
-  uint8_t row = 0;
-  uint8_t col = 0;
+//------------------------------------------------------------------------------
 
-  ResetMatrix();
+static bool ScanMatrix(void) {
+  bool keyDown = false;
+  uint8_t row = 0;
+  uint8_t col = 0;  
+  
+  ResetCounter();
   
   for(row=0; row<8; row++) {
     for(col=0; col<8; col++) {
       if((PIND & MD) == 0) {
         matrix[row] &= ~(1<<col);
+        keyDown = true;
       }
       else {
         matrix[row] |= 1<<col;
@@ -72,13 +89,120 @@ static void ScanMatrix(void) {
       ClockMatrix();
     }
   }
-  PORTB = matrix[0];
+  return keyDown;
 }
+
+//------------------------------------------------------------------------------
+
+static void RelayMatrix(void) {
+  // IMPLEMENTME
+}
+
+//------------------------------------------------------------------------------
+
+static void RelayKeyPress(key_t key) {
+  // IMPLEMENTME
+}
+
+//------------------------------------------------------------------------------
+
+static bool KeysEqual(key_t key, key_t other) {
+  return key.col == other.col && key.row == other.row;
+}
+
+//------------------------------------------------------------------------------
+
+static bool QueryKeyPress(key_t key) {
+
+  /* If the requested key is down, wait until it is released. */
+  
+  bool result = false;
+  
+  if(ScanMatrix() && (matrix[key.row] & 1<<key.col) == 0) {
+    _delay_ms(20);
+    if(ScanMatrix() && (matrix[key.row] & 1<<key.col) == 0) {
+      result = true;
+      while(ScanMatrix() && (matrix[key.row] & 1<<key.col) == 0);
+    }
+  }
+  return result;
+}
+
+//------------------------------------------------------------------------------
+
+static void ReadKeyPress(key_t* key) {
+
+  /* Wait until a key is down, then wait for release and report back
+   * which key was pressed.
+  */
+
+  uint8_t row = 0;
+  uint8_t col = 0;  
+  
+  while(!ScanMatrix());
+
+  _delay_ms(20);
+  
+  if(ScanMatrix()) {
+    for(row=0; row<8; row++) {
+      for(col=0; col<8; col++) {
+        if((matrix[row] & 1<<col) == 0) {
+          key->row = row;
+          key->col = col;
+          goto waitForKeyRelease;
+        }
+      }
+    }
+
+  waitForKeyRelease:
+    while(ScanMatrix() && (matrix[key->row] & 1<<key->col) == 0);
+  }
+}
+
+//------------------------------------------------------------------------------
 
 int main(void) {
   SetupHardware();
-  
-  while(1) ScanMatrix();
 
+  key_t key = { .col = 0, .row = 0 };
+  
+  while(true) {
+    
+    switch(STATE) {
+
+    case RELAY:
+      PORTB &= ~1;
+      if(QueryKeyPress(KEY_ARROW_LEFT)) {
+        STATE = COMMAND;
+      }
+      else {
+        RelayMatrix();
+      }
+      break;
+      
+    case COMMAND:
+      PORTB |= 1;
+
+      ReadKeyPress(&key);
+      
+      if(KeysEqual(key, KEY_ARROW_LEFT)) {
+        RelayKeyPress(KEY_ARROW_LEFT);
+        STATE = RELAY;
+      }        
+      else if(KeysEqual(key, KEY_3)) {
+
+        if(PINB & 1<<7) {
+          PORTB &= ~(1<<7);
+        } else {
+          PORTB |= 1<<7;          
+        }
+        STATE = RELAY;
+      }
+      else {
+        STATE = RELAY;
+      }
+      break;
+    }
+  }
   return 0;
 }
--- firmware/main.h
+++ firmware/main.h
@@ -1,9 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+typedef struct {
+  uint8_t col;
+  uint8_t row;
+} key_t;
+
+key_t KEY_ARROW_LEFT = { .col = 7, .row = 1 };
+key_t KEY_K          = { .col = 4, .row = 5 }; 
+key_t KEY_3          = { .col = 1, .row = 0 };
+
 static void SetupHardware(void);
 static void DisableJTAG(void);
+static void ResetCounter(void);
 static void ClockMatrix(void);
-static void ScanMatrix(void);
+static bool ScanMatrix(void);
+static void RelayMatrix(void);
+static bool KeysEqual(key_t key, key_t other);
+static void RelayKeyPress(key_t key);
+static bool QueryKeyPress(key_t key);
+static void ReadKeyPress(key_t* key);
 
 #endif // MAIN_H
